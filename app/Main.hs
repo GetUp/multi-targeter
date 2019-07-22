00c51e0d7ded36d8e109c7685df8670284a6d86d
Tim McEwan <tim@mcewan.it>
2019-07-22 21:05:01 +1000

    Tut

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,33 @@ module Main where
 
 import           AWSLambda.Events.APIGateway
 import           Control.Lens
-import           Data.Aeson
-import           Data.Aeson.Embedded
+import qualified Data.ByteString.Lazy.Internal as BSL
+import qualified Data.HashMap.Strict           as HMS
+import           Data.Text
+import qualified Data.Text.Lazy                as LazyText
+import qualified Data.Text.Lazy.Encoding       as LazyText
+import qualified Network.Wreq                  as Http
 
+main :: IO ()
 main = apiGatewayMain handler
 
-handler ::
-     APIGatewayProxyRequest (Embedded Value)
-  -> IO (APIGatewayProxyResponse (Embedded [Char]))
+htmlRes :: Int -> Text -> IO (APIGatewayProxyResponse Text)
+htmlRes status proxyBody =
+  pure $ htmlResWithNoBody status & responseBody ?~ proxyBody
+  where
+    htmlResWithNoBody :: Int -> APIGatewayProxyResponse Text
+    htmlResWithNoBody statusCode =
+      APIGatewayProxyResponse statusCode [("Content-Type", "text/html")] Nothing
+
+getProxyBody :: Http.Response BSL.ByteString -> IO Text
+getProxyBody resFromGivenUrl =
+  return . LazyText.toStrict . LazyText.decodeUtf8 $
+  resFromGivenUrl ^. Http.responseBody
+
+handler :: APIGatewayProxyRequest Text -> IO (APIGatewayProxyResponse Text)
 handler request = do
-  putStrLn "This should go to logs"
-  print $ request ^. requestBody
-  pure $ responseOK & responseBodyEmbedded ?~ "text"
+  let urlPath = HMS.lookup "url" $ request ^. agprqPathParameters
+  case urlPath of
+    Just path ->
+      (Http.get $ "https://" <> unpack path) >>= getProxyBody >>= htmlRes 200
+    Nothing -> htmlRes 500 "No path found"
