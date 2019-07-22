e34a5c19d75fbabc0f70f94ee93b8d2278ffc767
Tim McEwan <tim@mcewan.it>
2019-07-22 17:39:38 +1000

    Compilable/deployable, but not responding properly

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
 module Main where
 
-import Lib
+import           AWSLambda
+import           Prelude.Compat
 
-main :: IO ()
-main = someFunc
+import           Data.Aeson                 (Value, decode, encode)
+import           Data.Aeson.TH              (defaultOptions, deriveJSON)
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+data Response =
+  Response
+    { statusCode :: Int
+    , body       :: String
+    }
+  deriving (Show)
+
+$(deriveJSON defaultOptions ''Response)
+
+main = lambdaMain handler
+
+handler :: Value -> IO String
+handler evt = do
+  let reply = Response {statusCode = 200, body = "<_-.-_>I'm working!</_-.-_>"}
+  pure $ BL.unpack (encode reply)
