fc8ecdea1469c002662a8231d22df4fb5b6add75
Tim McEwan <tim@mcewan.it>
2019-07-29 18:20:33 +1000

    Remove redundant db setup

--- setup_database.sql
+++
@@ -1,9 +0,0 @@
-create table "public"."targets" (
-  "id" serial
-  , "name" text
-  , "number" text
-  , primary key ("id")
-);
-
-insert into "public"."targets" ("id" , "name" , "number")
-    values (1 , E'Sydney office' , E'61285994347') , (2 , E'Melbourne office' , E'61285994347');
