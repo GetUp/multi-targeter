ddc18333acc0377391ebc009d2371620af58998b
Tim McEwan <tim@mcewan.it>
2019-07-30 11:51:19 +1000

    Tidy handler

--- src/Handler.hs
+++ src/Handler.hs
@@ -21,27 +21,20 @@ handler :: APIGatewayProxyRequest Text -> IO (APIGatewayProxyResponse Text)
 handler request = do
   let appUrl = buildAppUrl request
   let urlPath = BS.unpackChars $ request ^. agprqPath
-  let params = buildParams request
-  case (urlPath, params) of
+  url <- dbUrl
+  conn <- connectPostgreSQL url
+  case (urlPath, buildParams request) of
     ("/connect", Params { campaignIdParam = Just campaignId
                         , fromNumberParam = Just fromNumber
                         , callUuidParam = Just callUuid
                         }) -> do
-      url <- dbUrl
-      conn <- connectPostgreSQL url
-      _ <-
-        execute
-          conn
-          "insert into callers (number, campaign_id, call_uuid, created_at) values (?, ?, ?, now())"
-          (fromNumber, wrap campaignId, callUuid)
+      _ <- execute conn insertCaller (fromNumber, wrap campaignId, callUuid)
       pure $ xmlResponse $ plivoResponse $ do
         speak "Welcome to the Test Campaign."
         speak
           "We will attempt to connect to you to one of their 40 offices. If someone picks up and you have conversation, afterwards we will ask you questions about how it went. You then have the option to try another office. Press the star key to hang up at any point during the conversation. Remember to be polite."
         redirect $ appUrl "/call"
     ("/call", _) -> do
-      url <- dbUrl
-      conn <- connectPostgreSQL url
       [(targetName, targetNumber)] <- query_ conn randomTarget :: IO [(Text, Text)]
       pure $ xmlResponse $ plivoResponse $ do
         speak $ "Calling the " <> targetName
@@ -51,11 +44,9 @@ handler request = do
         speak "The call has ended."
         redirect $ appUrl "/call"
     ("/disconnect", Params {callUuidParam = Just callUuid, durationParam = Just duration}) -> do
-      url <- dbUrl
-      conn <- connectPostgreSQL url
-      _ <- execute conn "update callers set ended_at = now(), duration = ? where call_uuid = ?" (duration, callUuid)
+      _ <- execute conn updateCaller (duration, callUuid)
       pure xmlResponseOk
-    (_, _) -> pure $ response404
+    (_, _) -> pure response404
 
 dbUrl :: IO BS.ByteString
 dbUrl = do
@@ -92,6 +83,12 @@ lookupBody request param = do
 
 randomTarget :: Query
 randomTarget = "select name, number from targets order by random() limit 1"
+
+insertCaller :: Query
+insertCaller = "insert into callers (number, campaign_id, call_uuid, created_at) values (?, ?, ?, now())"
+
+updateCaller :: Query
+updateCaller = "update callers set ended_at = now(), duration = ? where call_uuid = ?"
 
 xmlResponseOk :: APIGatewayProxyResponse body
 xmlResponseOk = APIGatewayProxyResponse 200 [("Content-Type", "text/xml")] Nothing
